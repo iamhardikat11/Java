51546b6c4821bd76fecf9ca19b31c3b5035d18dd
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-10-26 17:42:11 +0530

    Create Hello_World.java

---
+++ Basic/Hello_World.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class Solution{
+  public static void main(String args[]) throws IOException
+  {
+    System.out.println("Hello World");//Simple Java File to print "Hello World".
+  }
+}
